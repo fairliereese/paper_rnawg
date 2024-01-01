5b001856b63dcfae74b1e1a84b09df1640dbc498
fairliereese <fairliek@comcast.net>
2024-01-01 11:21:24 -0800

    update

--- proc_revisions/mane_utils.py
+++ proc_revisions/mane_utils.py
@@ -13,10 +13,10 @@ import swan_vis as swan
 p = os.path.dirname(os.path.dirname(os.getcwd()))
 sys.path.append(p)
 
-from .utils import *
-from .plotting import *
-# from utils import *
-# from plotting import *
+# from .utils import *
+# from .plotting import *
+from utils import *
+from plotting import *
 
 def get_exp_gene_subset(ab, min_tpm,
                         obs_col,

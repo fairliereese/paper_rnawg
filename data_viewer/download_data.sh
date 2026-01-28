60799f7f33c5810ecc7f1d01f305652948ec701f
fairliereese <fairliek@comcast.net>
2026-01-28 22:21:47 +0100

    update

--- data_viewer/download_data.sh
+++ data_viewer/download_data.sh
@@ -1,9 +1,9 @@
 # dl data and put into data directory
 mkdir data
-wget https://zenodo.org/records/18299002/files/human_triplets.h5 -o data/human_triplets.h5
-wget https://zenodo.org/records/18299002/files/mouse_triplets.h5 -o data/mouse_triplets.h5
-wget https://zenodo.org/records/18299002/files/human_swan_graph.p -o data/human_swan.p
-wget https://zenodo.org/records/18299002/files/mouse_swan_graph.p -o data/mouse_swan.p
+wget https://zenodo.org/records/18299002/files/human_triplets.h5 -O data/human_triplets.h5
+wget https://zenodo.org/records/18299002/files/mouse_triplets.h5 -O data/mouse_triplets.h5
+wget https://zenodo.org/records/18299002/files/human_swan_graph.p -O data/human_swan.p
+wget https://zenodo.org/records/18299002/files/mouse_swan_graph.p -O data/mouse_swan.p
 
 # pull docker image
 docker pull ghcr.io/fairliereese/encode4-viewer:latest
